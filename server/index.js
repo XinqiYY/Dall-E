64af37d936b87da68e6348d38c92c0c30f78f045
XinqiYY <xinqiy717595@gmail.com>
2024-02-15 15:22:01 -0800

    init routers for post and dalle

--- server/index.js
+++ server/index.js
@@ -3,12 +3,18 @@ import * as dotenv from 'dotenv';
 import cors from 'cors';
 
 import connectDB from './mongoDB/connect.js';
+import postRoutes from './routes/postRoutes.js';
+import dalleRoutes from './routes/dalleRoutes.js';
 
 dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json({ limit: '50mb' }));
+
+// create an api
+app.use('/api/v1/post', postRoutes);
+app.use('api/v1/dalle', dalleRoutes);
 
 app.get('/', async (req, res) => {
   res.send('Hello world');
